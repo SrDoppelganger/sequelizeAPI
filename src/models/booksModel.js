ab200aeaa909dea5da9a475eb6f00696c31ca289
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2024-11-28 18:11:37 -0300

    updated books model and updated clients models

--- src/models/booksModel.js
+++ src/models/booksModel.js
@@ -3,21 +3,28 @@ import db from '../db.js';
 
 export default db.define('book',{
   id:{
-
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
   },
   titulo:{
-
+    type: Sequelize.STRING,
+    allowNull: false
   },
   autor:{
-
+    type: Sequelize.STRING,
+    allowNull: false
   },
   genero:{
-
+    type: Sequelize.STRING,
+    allowNull: false
   },
   ano:{
 
   },
   disponivel:{
-
+    type: Sequelize.BOOLEAN,
+    allowNull: false
   }
 });
--- src/models/clientsModel.js
+++ src/models/clientsModel.js
@@ -3,21 +3,27 @@ import db from '../db.js';
 
 export default db.define('client',{
   id:{
-
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
   },
   nome:{
-
+    type: Sequelize.STRING,
+    allowNull: false
   },
   email:{
-
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
   },
   endereco:{
-
+    
   },
   telefone:{
 
   },
   CPF:{
-    
+
   }
 })
